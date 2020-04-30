1f5a04b71d55adcced7484a126772d6323870b09
Allen Smith <allen@whoi.edu>
2020-04-30 16:59:54 -0400

    creating seabird and serial instrument classes

---
+++ common/seabird_common.py
@@ -0,0 +1,6 @@
+from . import Serial_instrument
+
+class Seabird_instrument(Serial_instrument):
+	def __init__(self):
+		# Initialize shared Instrument superclass attributes...
+		super().__init__()
